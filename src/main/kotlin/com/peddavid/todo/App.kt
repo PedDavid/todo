94b2ab89819cccb5ab0b880512467adbf8247f64
PedDavid <peddavid96@outlook.com>
2017-04-26 17:29:21 +0100

    Initial commit of sample Kotlin Project with Test

---
+++ src/main/kotlin/com/peddavid/todo/App.kt
@@ -0,0 +1,3 @@
+package com.peddavid.todo
+
+fun main(args: Array<String>) = println("Hello World!!!")
---
+++ src/test/kotlin/com/peddavid/todo/AppTest.kt
@@ -0,0 +1,11 @@
+package com.peddavid.todo
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AppTest {
+    @Test fun twoPlusTwoIsFour() {
+        val twoPlusTwo = 2 + 2
+        assertTrue("Two Plus Two is Four", twoPlusTwo == 4)
+    }
+}
