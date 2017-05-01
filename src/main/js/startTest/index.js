6c928b73f01cca4aadabe59755840e4438414c26
PedDavid <peddavid96@outlook.com>
2017-05-01 23:37:31 +0100

    Added WebPack as Module Bundler, added Script build to Client Side Project that bundles a simple script

--- src/main/js/startTest/index.js
+++ src/main/js/startTest/index.js
@@ -1,3 +1,7 @@
-function main (){
-    console.log('Hello World')
+function component () {
+    const element = document.createElement('div');
+    element.innerHTML = 'Hello webpack'
+    return element;
 }
+
+document.body.appendChild(component());
