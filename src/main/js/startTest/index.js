d08f679f10b9489b65f6dc47ca30419888df673b
PedDavid <peddavid96@outlook.com>
2017-05-02 00:52:28 +0100

    Simple React example

--- src/main/js/startTest/index.js
+++ src/main/js/startTest/index.js
@@ -1,7 +1,11 @@
-function component () {
-  const element = document.createElement('div')
-  element.innerHTML = 'Hello webpack'
-  return element
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function HelloWorld (props) {
+  return React.createElement('h1', null, `Hello ${props.name} World`)
 }
 
-document.body.appendChild(component())
+ReactDOM.render(
+  React.createElement(HelloWorld, { name: 'React' }),
+  document.getElementById('react')
+)
