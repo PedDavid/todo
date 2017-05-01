1f051de7744abdc7a10efac834c29331c3cbc04b
PedDavid <peddavid96@outlook.com>
2017-05-01 23:59:29 +0100

    Added Standard Linter to dev dependencies and test Script that runs linter, changed js files to adhere to standard (2 spaces instead of 4, no semicolons and eof line)

--- src/main/js/startTest/webpack.config.js
+++ src/main/js/startTest/webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-};
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
