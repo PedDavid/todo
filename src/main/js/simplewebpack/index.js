455c7e8c3436fb1f4e6ee87717e6365a1ccd305c
PedDavid <peddavid96@outlook.com>
2017-05-06 22:29:44 +0100

    Added new sample project for test of last TODO

---
+++ src/main/js/simplewebpack/index.js
@@ -0,0 +1,4 @@
+const heading = document.createElement('h1')
+const headingText = document.createTextNode('Hello Web Pack World')
+heading.appendChild(headingText)
+document.getElementById('root').appendChild(heading)
