862a69705a6e816162c0be34a530a060e1eb5a1e
PedDavid <peddavid96@outlook.com>
2017-05-09 16:06:59 +0100

    Added Redux to project and simple redux usage

--- src/main/js/simplewebpack/index.js
+++ src/main/js/simplewebpack/index.js
@@ -2,8 +2,53 @@ import 'semantic-ui-css/semantic.min.css'
 import './style.scss'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import { Icon } from 'semantic-ui-react'
+import { createStore } from 'redux'
+
+// Action
+// const addTodo = { type: 'ADD_TODO', text: 'My Todo' }
+// const removeTodo = { type: 'REMOVE_TODO', id: 1 }
+// const toggleTodo = { type: 'TOGGLE_TODO', id: 1 }
+// const showCompletedTodo = {type: 'SET_VISIBILITY_FILTER', filter: 1 }
+
+// Action Creator
+const addTodo = text => ({ type: 'ADD_TODO', text })
+const removeTodo = id => ({ type: 'REMOVE_TODO', id })
+const toggleTodo = id => ({ type: 'TOGGLE_TODO', id })
+
+// Reducer (prevState, action) => newState
+function todoReducer (state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO': {
+      return [...state, { text: action.text, completed: false }]
+    }
+    case 'TOGGLE_TODO': {
+      return state.map((todo, index) => {
+        if (index === action.id) return Object.assign({}, todo, { completed: !todo.completed })
+        return todo
+      })
+    }
+    case 'REMOVE_TODO': {
+      return state.filter((todo, index) => index !== action.id)
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+// Create Store
+const store = createStore(todoReducer)
+
+// Sample flow
+store.dispatch(addTodo('Hello'))
+store.dispatch(addTodo('Redux'))
+store.dispatch(addTodo('World'))
+store.dispatch(addTodo('!'))
+store.dispatch(toggleTodo(3))
+store.dispatch(addTodo('Hello'))
+store.dispatch(removeTodo(4))
 
 const App = () =>
   <div>
@@ -11,7 +56,7 @@ const App = () =>
     <Icon loading name='spinner' />
   </div>
 
-ReactDOM.render(
+render(
   <App />,
   document.getElementById('root')
 )
